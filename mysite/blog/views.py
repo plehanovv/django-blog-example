bd1f6032665325debedcc79070c64cf9c03b8665
yaroslav <plehanov.kzn2@gmail.com>
2024-11-29 18:44:28 +0300

    finished dev

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # class PostListView(ListView):
@@ -110,7 +110,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body')).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector,
+                                              rank=SearchRank(search_vector, search_query)
+                                              ).filter(rank__gte=0.3).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
