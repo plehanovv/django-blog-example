a75d6229f6d1ff967b6b87ad4c4cd9e7efdbbacd
yaroslav <plehanov.kzn2@gmail.com>
2024-11-30 15:50:35 +0300

    created api views

--- mysite/blog_api/serializers.py
+++ mysite/blog_api/serializers.py
@@ -1,17 +1,14 @@
 from rest_framework import serializers
-from mysite.blog.models import Comment
+from mysite.blog.models import Post
 
 
-class CommentSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    body = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return Comment(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance.email)
-        instance.body = validated_data.get('content', instance.body)
-        instance.created = validated_data.get('created', instance.created)
-        return instance
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            "id",
+            "author",
+            "title",
+            "body",
+            "created",
+        )
+        model = Post
