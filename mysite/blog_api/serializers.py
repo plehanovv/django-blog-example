3e9fb6bedba49ef0a16006e5a6d964062fedb3d6
yaroslav <plehanov.kzn2@gmail.com>
2024-11-30 15:58:26 +0300

    configured api

--- mysite/blog_api/serializers.py
+++ mysite/blog_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from mysite.blog.models import Post
+from blog.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
--- mysite/blog_api/views.py
+++ mysite/blog_api/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from mysite.blog.models import Post
+from blog.models import Post
 from .serializers import PostSerializer
 
 
